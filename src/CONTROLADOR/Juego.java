4556e7ee18607038276d006d1f2c5a3412e58484
JoelCobachoSole <cobachosolejoel@alumnes.ilerna.com>
2025-04-25 19:00:47 +0200

    .
    .

--- src/CONTROLADOR/Juego.java
+++ src/CONTROLADOR/Juego.java
@@ -1,10 +1,26 @@
 package CONTROLADOR;
 
-public class Juego {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Juego extends Application {
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//System.out.println(getClass().getResource("/pantallaPrincipal.fxml"));
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/pantallaPrincipal.fxml"));
+	    Parent root = loader.load();
+
+	    Scene scene = new Scene(root);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("El Juego del Pingüino");
+	    primaryStage.show();
 	}
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
